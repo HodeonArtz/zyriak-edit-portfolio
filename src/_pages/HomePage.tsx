c9c60a61de5e9c0f2a13215c08a89b4631a08266
Glen Marti Diciano Ibias <martidicianolold@gmail.com>
2026-07-21 21:37:28 +0200

    feat(glossiness): add glossy containers to rest of sections

--- src/_pages/HomePage.tsx
+++ src/_pages/HomePage.tsx
@@ -7,36 +7,40 @@ const HomePage = () => {
       <GlossyContainer>
         <ProfileSection />
       </GlossyContainer>
-      <section>
-        <h2>My works</h2>
-        <img src="" alt="edit-1" />
-        <img src="" alt="edit-2" />
-        <img src="" alt="edit-3" />
-        <img src="" alt="edit-4" />
-        <img src="" alt="edit-5" />
-        <img src="" alt="edit-6" />
-        <img src="" alt="edit-7" />
-        <img src="" alt="edit-8" />
-        <img src="" alt="edit-9" />
-        <img src="" alt="edit-10" />
-        <img src="" alt="edit-11" />
-        <img src="" alt="edit-12" />
-        <div className="goto-socials-button-container">
-          <button>Go watch my other works!</button>
+      <GlossyContainer>
+        <div>
+          <h2>My works</h2>
+          <img src="" alt="edit-1" />
+          <img src="" alt="edit-2" />
+          <img src="" alt="edit-3" />
+          <img src="" alt="edit-4" />
+          <img src="" alt="edit-5" />
+          <img src="" alt="edit-6" />
+          <img src="" alt="edit-7" />
+          <img src="" alt="edit-8" />
+          <img src="" alt="edit-9" />
+          <img src="" alt="edit-10" />
+          <img src="" alt="edit-11" />
+          <img src="" alt="edit-12" />
+          <div className="goto-socials-button-container">
+            <button>Go watch my other works!</button>
+          </div>
         </div>
-      </section>
-      <section>
-        <h2>My socials</h2>
-        <button>YouTube</button>
-        <button>Instagram</button>
-        <button>TikTok</button>
-        <button>davidpetersen.0318@gmail.com</button>
-        <div className="credits-section">
-          <p>
-            Website designed and built by: <img alt="hdn logo" />
-          </p>
+      </GlossyContainer>
+      <GlossyContainer>
+        <div>
+          <h2>My socials</h2>
+          <button>YouTube</button>
+          <button>Instagram</button>
+          <button>TikTok</button>
+          <button>davidpetersen.0318@gmail.com</button>
+          <div className="credits-section">
+            <p>
+              Website designed and built by: <img alt="hdn logo" />
+            </p>
+          </div>
         </div>
-      </section>
+      </GlossyContainer>
     </>
   );
 };
