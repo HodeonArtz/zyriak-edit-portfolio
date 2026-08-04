9e5aa47bffe68a8f9ec43145159508ff66e5ee34
Glen Marti Diciano Ibias <martidicianolold@gmail.com>
2026-08-05 01:11:03 +0200

    feat(animation): make smooth scrolling with navbar and rest of page

--- src/_components/Navbar/Navbar.tsx
+++ src/_components/Navbar/Navbar.tsx
@@ -1,7 +1,50 @@
 import "./Navbar.css";
 import GlossyNavLink from "./GlossyNavLink";
 import VerticalSeparator from "../VerticalSeparator/VerticalSeparator";
+
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { Flip } from "gsap/Flip";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, Flip);
+
 const Navbar = () => {
+  useGSAP(() => {
+    gsap.set(".navbar__wrapper", {
+      paddingTop: "2.0625em",
+      paddingBottom: "2.0625em",
+    });
+    const navbarWidthAnim = gsap.to(".navbar", {
+      scrollTrigger: {
+        trigger: "section:first-child",
+        start: "-=100 top",
+        end: "top top",
+        scrub: 0.5,
+      },
+      ease: "power2.inOut",
+      duration: 0.2,
+      width: "100%",
+      borderRadius: "0px",
+    });
+    const navbarPaddingAnim = gsap.to(".navbar__wrapper", {
+      scrollTrigger: {
+        trigger: ".navbar__wrapper",
+        start: "top top",
+        end: "bottom top",
+        scrub: 0.5,
+      },
+      ease: "none",
+      paddingTop: "0em",
+      paddingBottom: "4.125em",
+    });
+    return () => {
+      navbarWidthAnim.kill();
+      navbarPaddingAnim.kill();
+    };
+  }, []);
+
   return (
     <div className="navbar__wrapper">
       <div className="navbar__content">
--- src/_layouts/ResponsiveLayout.tsx
+++ src/_layouts/ResponsiveLayout.tsx
@@ -2,7 +2,23 @@ import { Outlet } from "react-router";
 import "./ResponsiveLayout.css";
 import Navbar from "../_components/Navbar/Navbar";
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(useGSAP, ScrollSmoother);
+
 const ResponsiveLayout = () => {
+  useGSAP(() => {
+    const smoother = ScrollSmoother.create({
+      wrapper: ".wrapper",
+      content: ".container",
+      smooth: 1,
+    });
+    return () => {
+      smoother.kill();
+    };
+  }, []);
   return (
     <div className="wrapper">
       <Navbar />
