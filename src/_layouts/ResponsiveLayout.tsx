01341c3c310a573fdda89088608a427303e2b0c7
Glen Marti Diciano Ibias <martidicianolold@gmail.com>
2026-08-11 15:09:15 +0200

    feat(bg): add mix blend layers

--- src/_layouts/ResponsiveLayout.tsx
+++ src/_layouts/ResponsiveLayout.tsx
@@ -27,6 +27,16 @@ const ResponsiveLayout = () => {
       <main className="container">
         <div className="bg-wrapper">
           <img
+            src="vectors/dorfic-ellipse-color-burn-dark.svg"
+            alt="dorfic-ellipse-color-burn-dark.svg"
+            className="bg__ellipse-burn-dark"
+          />
+          <img
+            src="vectors/dorfic-ellipse-color-burn.svg"
+            alt="dorfic-ellipse-color-burn.svg"
+            className="bg__ellipse-burn"
+          />
+          <img
             src="vectors/dorfic-gradient-bg.svg"
             alt="dorfic-gradient-bg.svg"
             className="parallax-bg"
