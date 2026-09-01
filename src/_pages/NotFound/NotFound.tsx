a00045515763ffa38c3c7659ebb95f09ddce924f
Glen Marti Diciano Ibias <martidicianolold@gmail.com>
2026-09-01 17:31:14 +0200

    feat(404-page): add 404 page content

--- src/_pages/NotFound/NotFound.tsx
+++ src/_pages/NotFound/NotFound.tsx
@@ -1,8 +1,37 @@
+import Button from "../../_components/Button/Button";
+import GlossyContainer from "../../_components/GlossyContainer/GlossyContainer";
 import useNavbarContext from "../../_components/Navbar/_hooks/useNavbarContext";
+import Title from "../../_components/Title/Title";
+import "./NotFound.css";
 
 const NotFound = () => {
   useNavbarContext(false);
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found__layout">
+      <GlossyContainer>
+        <div className="not-found__section">
+          <div className="not-found__heading">
+            <Title
+              as="h1"
+              className="not-found__title"
+              aria-label="Error 404 - Not Found"
+            >
+              404
+            </Title>
+            <Title as="h2" className="not-found__subtitle">
+              not found
+            </Title>
+          </div>
+          <div className="not-found__description">
+            <p>The requested page was not found. However... You can always:</p>
+            <Button as="anchor" href="/">
+              Return to homepage
+            </Button>
+          </div>
+        </div>
+      </GlossyContainer>
+    </div>
+  );
 };
 
 export default NotFound;
