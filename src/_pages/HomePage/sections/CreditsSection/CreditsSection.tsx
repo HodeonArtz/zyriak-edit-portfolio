c75677d12f68d87046af7af60c2f62996a1836ab
Glen Marti Diciano Ibias <martidicianolold@gmail.com>
2026-09-03 02:41:51 +0200

    feat(credits): set layout for mobile for better readability

--- src/_pages/HomePage/sections/CreditsSection/CreditsSection.tsx
+++ src/_pages/HomePage/sections/CreditsSection/CreditsSection.tsx
@@ -6,17 +6,20 @@ const CreditsSection = () => {
         <span className="credits__brand-logo">Zyriak</span>
         <p>© {new Date().getFullYear()} Zyriak</p>
       </div>
-      <div className="credits__producer-section">
+      <a
+        href="https://github.com/HodeonArtz"
+        target="_blank"
+        className="credits__producer-section"
+      >
         <p>Website designed and built by:</p>
-        <a href="https://github.com/HodeonArtz" target="_blank">
-          <img
-            className="credits__producer-logo"
-            alt="hdn logo"
-            src="img/hodeon-logo-80x80.png"
-            title="HodeonArtz"
-          />
-        </a>
-      </div>
+
+        <img
+          className="credits__producer-logo"
+          alt="hdn logo"
+          src="img/hodeon-logo-80x80.png"
+          title="HodeonArtz"
+        />
+      </a>
     </div>
   );
 };
